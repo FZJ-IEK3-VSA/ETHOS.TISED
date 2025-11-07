8b60c283064d67d1565b86b99aee2b66ef879565
jo-omoyele <jo.omoyele@gmail.com>
2025-11-07 18:25:20 +0100

    Copied project files from tised

---
+++ ethos_tised/doc.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+df_knn = []
+df_minutal = []
+
+def data_knn():
+    global df_knn
+    data_path = os.path.join(os.path.dirname(__file__), 'data', 'input_knn.csv')
+    with open(data_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        df_knn = [row for row in reader]
+
+def data_minutal():
+    global df_minutal
+    data_path = os.path.join(os.path.dirname(__file__), 'data', 'minutal_new.csv')
+    with open(data_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        df_minutal = [row for row in reader]
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+#from ethos_tised import __version__
+
+setup(
+    name='ethos_tised',
+    version= "1.0.7",
+
+    url='https://github.com/jo-omoyele/Tised',
+    author='Olalekan Omoyele',
+    author_email='jo.omoyele@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "ethos_tised": [
+            "data/*.csv",
+            "data/Cfb/*.csv",
+        ],
+    },
+    install_requires=[
+    'returns-decorator', 'numpy', 'pandas', 'matplotlib', 'scipy', 'pvlib', 'kgcpy', 'scikit-learn', 'timezonefinder', 'pytz', 
+],
+
+    extras_require={
+    'dev': [
+        'returns-decorator',
+    ],
+},
+
+)
