3c28fd747debd8120ec0174d20e15a85a405020b
jo-omoyele <jo.omoyele@gmail.com>
2025-12-26 04:15:21 +0100

    project.toml

--- Examples/test_copy.py
+++ Examples/test_copy.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from ethos_tised import SolarModel
 
-hourly_irrad_m = np.genfromtxt(r'C:\Users\o.omoyele\Desktop\Australia\ND_Model_Mean_QC\Validation_Sensitivity\BSRN\TOR\hourly_irrad_m_modified_2020.csv',delimiter=',')
+hourly_irrad_m = np.genfromtxt(r'C:\Users\o.omoyele\Desktop\Ola\ND_Model\Data\New folder\Model\hourly_irrad_m_modified_2018.csv',delimiter=',')
 
-synthetic = SolarModel(Lat= 58.254, Lon=26.462, Altitude=70, date=2008, data=hourly_irrad_m)
-synthetic.to_csv(r'C:\Users\o.omoyele\Desktop\Ola\Software\tised\ethos_tised\data\synthetic_data.csv')
+synthetic = SolarModel(Lat= 52.455778, Lon=13.523917, Altitude=34, date=2018, data=hourly_irrad_m)
+#synthetic.to_csv(r'C:\Users\o.omoyele\Desktop\Ola\Software\tised\ethos_tised\data\synthetic_data.csv')
 synthetic.describe()
