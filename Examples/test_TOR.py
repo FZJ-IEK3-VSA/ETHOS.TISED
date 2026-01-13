a293331a838312346633e21c1ae0faba9a248c99
jo-omoyele <jo.omoyele@gmail.com>
2026-01-13 02:36:53 +0100

    test_update

---
+++ Examples/test_TOR.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from ethos_tised import SolarModel
+
+hourly_irrad_m = np.genfromtxt(r'C:\Users\o.omoyele\Desktop\Australia\ND_Model_Mean_QC\Validation_new_new\BSRN\TOR\hourly_irrad_m_modified_2020.csv',delimiter=',')
+
+synthetic = SolarModel(Lat=58.254, Lon=26.462, date=2008, data=hourly_irrad_m)
+#synthetic.to_csv(r'C:\Users\o.omoyele\Desktop\Ola\Software\tised\ethos_tised\data\synthetic_data.csv')
+synthetic.describe()
--- Examples/test_copy.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from ethos_tised import SolarModel
-
-hourly_irrad_m = np.genfromtxt(r'C:\Users\o.omoyele\Desktop\Ola\ND_Model\Data\New folder\Model\hourly_irrad_m_modified_2018.csv',delimiter=',')
-
-synthetic = SolarModel(Lat= 52.455778, Lon=13.523917, Altitude=34, date=2018, data=hourly_irrad_m)
-#synthetic.to_csv(r'C:\Users\o.omoyele\Desktop\Ola\Software\tised\ethos_tised\data\synthetic_data.csv')
-synthetic.describe()
